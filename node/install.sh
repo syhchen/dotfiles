d4e74a33fce1069d837552a55d0ddbb5c815bb09
Stephen Chen <hello@stephenc.me>
2017-09-27 18:04:24 -0700

    Removes inferno now that React has moved to MIT license

--- node/install.sh
+++ node/install.sh
@@ -9,8 +9,5 @@ echo "\033[1;32mInstalling create-react-app...\033[0m"
 
 npm install -g create-inferno-app
 
-echo "\033[1;32mInstalling create-inferno-app...\033[0m"
-npm install -g create-react-app
-
 echo "\033[1;32mInstalling surge...\033[0m"
 npm install -g surge
